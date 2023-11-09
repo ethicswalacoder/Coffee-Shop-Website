2d9fd517385849996c69322a153739bd0367e374
Sahoo <artbangla21@gmail.com>
2023-11-09 11:51:41 +0530

    this is my first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+
+function App() {
+
+
+  return (
+    <>
+    <Header/>
+    <Home/>
+    </>
+  )
+}
+
+export default App
