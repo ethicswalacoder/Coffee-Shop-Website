47bb01dcf0ec7adaf3370c00ce8879d2f1e3876b
Sahoo <artbangla21@gmail.com>
2023-11-19 21:24:19 +0530

    gallery section is completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 import About from './components/about/About';
 import Choose from './components/choose/Choose';
 import Features from './components/features/Features';
+import Gallery from './components/gallery/Gallery';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Menu from './components/menu/Menu';
+import Stats from './components/stats/Stats';
 
 function App() {
 
@@ -18,6 +20,8 @@ function App() {
     <Features/>
     <Menu/>
     <Choose/>
+    <Stats/>
+    <Gallery/>
     </>
   )
 }
